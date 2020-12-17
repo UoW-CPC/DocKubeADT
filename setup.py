013ec07c202828cee622935c17e48ffe0373e4bb
jaydesl <jay.deslauriers@gmail.com>
2020-12-17 14:53:51 +0000

    Bump minor version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="k8s2adt",
     description="Translate from k8s manifests to a MiCADO ADT",
-    version="0.1.0",
+    version="0.1.1",
     author="Jay DesLauriers",
     packages=find_packages(exclude=['tests']),
     install_requires=["ruamel.yaml", "click"],
