89b5b1682533625682fec6ac0d502fc262e00132
jaydesl <jay.deslauriers@gmail.com>
2020-12-17 14:53:11 +0000

    Exclude tests from wheel

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ setup(
     description="Translate from k8s manifests to a MiCADO ADT",
     version="0.1.0",
     author="Jay DesLauriers",
-    packages=find_packages(),
-    include_package_data=True,
+    packages=find_packages(exclude=['tests']),
     install_requires=["ruamel.yaml", "click"],
 
     python_requires=">=3.6",
