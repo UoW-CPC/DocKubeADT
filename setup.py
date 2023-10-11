7db6284d44aea391bfade97607655d9023340aa7
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2023-10-11 08:49:31 +0100

    chore: bump version for release

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="dockubeadt",
     description="Translate Docker compose and k8s manifests to a MiCADO ADT",
-    version="0.1.1",
-    author="Resmi Arjun",
+    version="0.2.0",
+    author="Resmi Arjun / Jay DesLauriers",
     packages=find_packages(exclude=['tests']),
     install_requires=["ruamel.yaml", "click"],
 
