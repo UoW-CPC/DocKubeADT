9dff9eb1bcc2c8534cfbc94515d5d0d6c9b9f7ff
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2023-11-15 14:19:20 +0000

    tests: add configmap test

--- tests/test_translation.py
+++ tests/test_translation.py
@@ -1,8 +1,8 @@
 import tempfile
 
 import pytest
-from ruamel.yaml import YAML
 
+from dockubeadt.translator import translate, translate_dict
 from dockubeadt.utils import load_yaml, dump_yaml
 
 
@@ -40,3 +40,29 @@ def test_compose_translation():
     data = translate("tests/data/docker-compose.yaml")
     nodes = data["topology_template"]["node_templates"]
     assert all(["db-service" in nodes, "db-deployment" in nodes])
+
+def test_configmap_creation():
+
+    config = [
+        {
+        "file_path": "/var/etc/mysql/my.cnf",
+        "file_content": "[MYSQL]\nsettings=True",
+        }
+    ]
+
+    with open("tests/data/docker-compose.yaml") as file:
+        data = load_yaml(file)
+    topo_tpl = load_yaml(translate_dict("docker-compose", data, config))
+    nodes = topo_tpl["node_templates"]
+    db_node = nodes["db-deployment"]
+    spec = db_node["interfaces"]["Kubernetes"]["create"]["inputs"]["spec"]["template"]["spec"]
+    cf_vol = spec["volumes"][-1]["configMap"]
+    cf_mount = spec["containers"][0]["volumeMounts"][-1]
+    assert all(
+        [
+            "my-cnf" in nodes,
+            cf_vol["name"] == "my-cnf",
+            cf_mount["subPath"] == "my.cnf",
+            cf_mount["mountPath"] == "/var/etc/mysql/my.cnf"
+        ]
+    )
